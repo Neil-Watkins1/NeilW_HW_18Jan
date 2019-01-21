a33c1e7b855128f10b3874551e4c2edc078bb67c
Neil Watkins <neil.watkins1@sky.com>
2019-01-21 08:34:13 +0000

    Added room till

--- Karaoke_specs/guest_spec.rb
+++ Karaoke_specs/guest_spec.rb
@@ -6,7 +6,7 @@ require_relative("../guest")
 class TestGuest < MiniTest::Test
 
   def setup
-    @guest = Guest.new("Jim", 35)
+    @guest = Guest.new("Jim", 35, "Lets do It")
   end
 
   def test_guest_has_name
@@ -15,6 +15,15 @@ class TestGuest < MiniTest::Test
 
   def test_guest_has_money
     assert_equal(35, @guest.wallet)
+  end
+
+  def test_guest_has_favorite_song
+    assert_equal("Lets do It", @guest.fav_song)
+  end
+
+  def test_guest_can_spend_money
+    @guest.spend_money(5)
+    assert_equal(30, @guest.wallet)
   end
 
 end
